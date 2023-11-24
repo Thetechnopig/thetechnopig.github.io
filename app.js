e6cf560016487544617335e43611bfddc0ae59af
Thetechnopig <indesweb@gmail.com>
2023-11-24 15:12:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const gameObjects = {
 // Initialize Leaflet map
 const map = L.map('map').setView([37.8, -96], 4);
 
-const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const osm = L.tileLayer('https://thetechnopig.github.io/assets/img/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
